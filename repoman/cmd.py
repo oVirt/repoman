4c5eaf08aca83095566d7b8a843e563d4ead216a
David Caro <dcaroest@redhat.com>
2014-12-16 13:38:19 +0100

    First working version
    At this point the following are implemented:
    
    Stores:
      * RPM (is a group of yum repos by distribution, including srcrpms and source
      code tarballs)
      * ISO
    
    Filters:
      * latest
      * name
    
    Sources:
      * conf
      * dir
      * jenkins
      * koji/brew
      * url
    
    There are no tests yet, but the infrastructure to add them and run them is there.
    
    Also adapted to the new 'standard' build/test POC:
    
    * build_artifacts:
        script to generate the artifacts (rpms in this case), it creates a
        directory 'exported-artifacts' in the same directory where it's run with
        the artifacts that should be archived.
    
    * build_artifacts.req[.$dist]
        plain text files with the package names of the requirements needed to
        build the artifacts, if the one with the suffix $dist exists it will be
        used (if matches), if not, it will use the .req one.
    
    * check_merged/patch
        script to run when a patch is merged/created
    
    * check_merged/patch.req[.$dist]
        same as build_artifacts.req, but for the check_merged/patch scripts
    
    Change-Id: Ic49bab1cb657c4156ac38bdb27c9c3fe1a118b1d
    Signed-off-by: David Caro <dcaroest@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/36224

---
+++ repoman/cmd.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+"""
+This program is a helper to repo management
+
+Started as specific for rpm files, but was modified to be able to support
+different types of artifacts
+"""
+import argparse
+import logging
+from getpass import getpass
+
+from urllib3 import connectionpool
+
+from .common.config import Config
+from .common.repo import Repo
+from .common.stores import STORES
+from .common.sources import SOURCES
+
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-n', '--noop', action='store_true')
+    parser.add_argument('-c', '--config', action='store', default=None,
+                        help='Configuration file to use')
+    parser.add_argument('-t', '--temp-dir', action='store', default=None,
+                        help='Temporary directory to use, will generate it if '
+                        'not passed')
+    parser.add_argument(
+        '-s', '--stores', required=False, default=','.join(STORES.keys()),
+        help='Store classes to take into account when loading the '
+        'repo. Available ones are %s' % ', '.join(STORES.keys()))
+    parser.add_argument('dir', help='Directory of the repo.')
+    parser.add_argument('-k', '--key', required=False,
+                        help='Path to the key to use when signing, will '
+                        'not sign any rpms if not passed.')
+    parser.add_argument('--passphrase', required=False,
+                        help='Passphrase to unlock the singing key')
+    parser.add_argument('--with-sources', required=False, action='store_true',
+                        help='Generate the sources tree.')
+    repo_subparser = parser.add_subparsers(dest='repoaction')
+    add_rpm = repo_subparser.add_parser('add', help='Add an artifact')
+    add_rpm.add_argument(
+        '-t', '--temp-dir', action='store', default=None,
+        help='Temporary dir to use when downloading artifacts'
+    )
+    add_rpm.add_argument(
+        'artifact_source', nargs='+',
+        help=(
+            'An artifact source to add, it can be one of: '
+            + ', '.join(', '.join(source.formats_list())
+                        for source in SOURCES.itervalues())
+        )
+    )
+
+    generate_src = repo_subparser.add_parser(
+        'generate-src',
+        help='Populate the src dir with the tarballs from the src.rpm '
+        'files in the repo')
+    generate_src.add_argument('-p', '--with-patches', action='store_true',
+                              help='Include the patch files')
+
+    repo_subparser.add_parser(
+        'createrepo',
+        help='Run createrepo on each distro repository.')
+
+    remove_old = repo_subparser.add_parser(
+        'remove-old',
+        help='Remove old versions of packages.')
+    remove_old.add_argument('-k', '--keep', action='store',
+                            default=1, help='Number of versions to '
+                            'keep')
+
+    repo_subparser.add_parser(
+        'sign-rpms',
+        help='Sign all the packages.')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.root.level = logging.DEBUG
+        #  we want connectionpool debug logs
+        connectionpool.log.setLevel(logging.DEBUG)
+        logging.debug('Enabled verbose mode')
+    else:
+        logging.basicConfig(level=logging.INFO)
+        logger.root.level = logging.INFO
+        #  we don't want connectionpool info logs
+        connectionpool.log.setLevel(logging.WARN)
+
+    if args.config:
+        config = Config(path=args.config)
+    else:
+        config = Config()
+
+    if args.dir.endswith('/'):
+        path = args.dir[:-1]
+    else:
+        path = args.dir
+    # handle the temporary dir setting
+    if args.temp_dir:
+        config.set('temp_dir', args.temp_dir)
+    # handle the signing_key and passphrase
+    if args.key:
+        config.set('signing_key', args.key)
+        config.set('signing_passphrase', args.passphrase)
+    if args.stores:
+        config.set('stores', args.stores)
+    if args.with_sources:
+        config.set('with_sources', 'true')
+    if config.get('signing_key', '') \
+       and config.get('signing_passphrase') == 'ask':
+        passphrase = getpass('Enter key passphrase: ')
+        config.set('signing_passphrase', passphrase)
+
+    # The signing key must be set prior to loading the repo
+    if args.repoaction == 'sign-rpms':
+        if not config.get('signing_key', ''):
+            config.set('signing_key', raw_input('Path to the signing key: '))
+        if config.get('signing_key', '') \
+           and not config.get('signing_passphrase') \
+           or config.get('signing_passphrase') == 'ask':
+            passphrase = getpass('Enter key passphrase: ')
+            config.set('signing_passphrase', passphrase)
+
+    repo = Repo(path=path, config=config)
+    logger.info('')
+    if args.repoaction == 'add':
+        logger.info('Adding artifacts to the repo %s', repo.path)
+        for art_src in args.artifact_source:
+            repo.add_source(art_src)
+        logger.info('')
+        repo.save()
+    elif args.repoaction == 'generate-src':
+        config.set('with_sources', 'true')
+    elif args.repoaction == 'remove-old':
+        repo.delete_old(keep=int(args.keep), noop=args.noop)
+    repo.save()
