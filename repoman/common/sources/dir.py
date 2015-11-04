4da25a12440a79d215b2ef2b3a5ee4043b46fd81
David Caro <dcaroest@redhat.com>
2015-11-04 20:10:58 +0100

    Fixed issue with relative path dirs
    Refactored the dir source class also a little, so it's more readable
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1277852
    Change-Id: I5c0a72ec88f9b0d3ede338b7dce94f60371485af
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- repoman/common/sources/dir.py
+++ repoman/common/sources/dir.py
@@ -47,33 +47,60 @@ class DirSource(ArtifactSource):
             "dir:repo_path"
         )
 
+    @staticmethod
+    def is_allowed(path, allowed_paths):
+        if allowed_paths and not any(
+            matched_path
+            for matched_path in allowed_paths
+            if path.startswith(matched_path)
+        ):
+            return False
+        return True
+
+    def resolve_path(self, path):
+        if os.path.isabs(path) and os.path.isdir(path):
+            return path
+
+        abs_path = os.path.abspath(path)
+        if os.path.isdir(abs_path):
+            return abs_path
+
+        allowed_paths = self.config.getarray('allowed_dir_paths')
+        for allowed_path in allowed_paths:
+            full_path = os.path.abspath(
+                os.path.join(allowed_path, path)
+            )
+            if os.path.isdir(full_path):
+                return full_path
+
+        raise IOError('No such directory in the path: %s' % path)
+
+    def check_if_allowed(self, path):
+        allowed_paths = self.config.getarray('allowed_dir_paths')
+        if not self.is_allowed(path, allowed_paths):
+            error_msg = 'Source %s outside the base path' % path
+            logger.error(error_msg + '\nAllowed paths: %s', allowed_paths)
+            raise IOError(error_msg)
+
     def expand(self, source_str):
         orig_source_str = source_str
-        allowed_paths = self.config.getarray('allowed_dir_paths')
         if source_str.startswith('dir:'):
             source_str = source_str.split(':', 1)[-1]
+        elif (
+            not os.path.isdir(source_str)
+            and has_store(source_str, self.stores)
+        ):
+            return '', [source_str]
         # get rid of any trailing filters
-        source_str, filters_str = split(source_str, ':', 1)
-        # handle relative paths, that is, the ones relative to the
-        # base_repo_path
-        if not source_str.startswith('/'):
-            for path in allowed_paths:
-                full_source_str = os.path.abspath(
-                    path + '/' + source_str
-                )
-                if os.path.isdir(full_source_str):
-                    source_str = full_source_str
-                    break
-            else:
-                return '', [orig_source_str]
-        if allowed_paths and not any(path for path in allowed_paths
-                                     if source_str.startswith(path)):
-            logging.error('Allowed paths: %s', allowed_paths)
-            raise Exception('Source %s outside the base path' % source_str)
-        if not os.path.isdir(source_str) \
-           and has_store(source_str, self.stores):
-            return filters_str, [source_str]
+        source_path, filters_str = split(source_str, ':', 1)
+        try:
+            source_path = self.resolve_path(source_path)
+        except IOError:
+            logger.debug('Skipping %s', orig_source_str)
+            return '', []
+        logger.debug('Resolved path: %s', source_path)
+        self.check_if_allowed(source_path)
         return (
             filters_str,
-            find_recursive(source_str, lambda x: has_store(x, self.stores))
+            find_recursive(source_path, lambda x: has_store(x, self.stores))
         )
--- tests/functional/dir.bats
+++ tests/functional/dir.bats
@@ -3,6 +3,7 @@
 load helpers
 
 TEST_DIR1=fixtures/testdir1
+TEST_DIR2=fixtures/testdir2
 TEST_DIR1_EXPECTED_PATHS=(
     rpm/fc21/x86_64/unsigned_rpm-1.1-1.fc21.x86_64.rpm
     rpm/fc21/x86_64/signed_rpm-1.0-1.fc21.x86_64.rpm
@@ -15,7 +16,30 @@ TEST_DIR1_EXPECTED_PATHS=(
     local repo
     repo="$BATS_TMPDIR/myrepo"
     rm -rf "$BATS_TMPDIR/myrepo"
-    repoman "$repo" add "dir:$BATS_TEST_DIRNAME/$TEST_DIR1"
+    repoman --verbose "$repo" add "dir:$BATS_TEST_DIRNAME/$TEST_DIR1"
+    for artifact in "${TEST_DIR1_EXPECTED_PATHS[@]}"; do
+        helpers.is_file "$repo/$artifact"
+    done
+}
+
+
+@test "source.dir: Add recursively a dir, full path" {
+    local repo
+    repo="$BATS_TMPDIR/myrepo"
+    rm -rf "$BATS_TMPDIR/myrepo"
+    repoman --verbose "$repo" add "dir:$BATS_TEST_DIRNAME/$TEST_DIR2"
+    for artifact in "${TEST_DIR1_EXPECTED_PATHS[@]}"; do
+        helpers.is_file "$repo/$artifact"
+    done
+}
+
+
+@test "source.dir: Add recursively a dir, relative path" {
+    local repo
+    repo="$BATS_TMPDIR/myrepo"
+    rm -rf "$BATS_TMPDIR/myrepo"
+    cd "$BATS_TEST_DIRNAME"
+    repoman --verbose "$repo" add "dir:$TEST_DIR2"
     for artifact in "${TEST_DIR1_EXPECTED_PATHS[@]}"; do
         helpers.is_file "$repo/$artifact"
     done
