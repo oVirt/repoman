e12f00d319c3da0527849817cab7fd1ecb89500a
Sandro Bonazzola <sbonazzo@redhat.com>
2017-10-11 16:31:26 +0200

    automation: fix check-merged
    aligned check-merged with check-patch
    
    Change-Id: I2ff2a8d4d54a40c30f5d8efcbed61d7245cfb14d
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
+
 if rpm --eval "%dist" | grep -qFi 'el6'; then
     # On EL6 there's no python2.7
     sed -i "s:python2.7:python2.6:" tox.ini
 fi
+
 
 echo "######################################################################"
 echo "#  Unit/static checks"
@@ -28,13 +30,28 @@ echo "#"
 
 shopt -s extglob
 if which yum-deprecated &>/dev/null; then
-    yum-deprecated install exported-artifacts/!(*src).rpm
+    yum-deprecated install -y exported-artifacts/!(*src).rpm
 else
-    yum install exported-artifacts/!(*src).rpm
+    yum install -y exported-artifacts/!(*src).rpm
 fi
 repoman -h
 
 echo "#"
 echo "# Installation OK"
+echo "######################################################################"
+
+echo "######################################################################"
+echo "#  Generating report"
+echo "#"
+
+cat <<EOR > exported-artifacts/index.html
+<html><body>
+<a href="coverage/functional/coverage.total.html/index.html">
+    Coverage report
+</a>
+EOR
+
+echo "#"
+echo "# Report generated"
 echo "######################################################################"
 
